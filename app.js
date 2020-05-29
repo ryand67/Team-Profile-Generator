5201aba2974b71d598b2c1aa08976811d2f8461e
ryand67 <rwdevenney@gmail.com>
2020-05-28 22:49:04 -0400

    Intern class

--- app.js
+++ app.js
@@ -115,8 +115,8 @@ const checkForOther = (employee) => {
     })
 }
 
+//Begin app
 inquirerEmployee();
-
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
